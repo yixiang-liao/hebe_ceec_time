d733d7dfe45379dd71c8219582e1317baa56f164
yixiang <82229244+yixiang-liao@users.noreply.github.com>
2022-12-04 22:25:02 +0800

    初始化

--- js/showmusic.js
+++
@@ -1,9 +0,0 @@
-let btn=document.querySelector("#showM");
-let infoModal=document.querySelector("#infoModal");
-let close=document.querySelector("#close");
-btn.addEventListener("click", function(){
-  infoModal.showModal();
-})
-close.addEventListener("click", function(){
-  infoModal.close();
-})// JavaScript Document
